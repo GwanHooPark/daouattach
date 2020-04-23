bde00b7438e58fb6032a25c6ff2367ad02db3709
GwanHooPark <hooho.dev@gmail.com>
2020-04-23 20:35:07 +0900

    electron-tabs 추가

--- src/background.js
+++ src/background.js
@@ -17,8 +17,10 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
-    nodeIntegration: true
+        nodeIntegration: true,
+        webviewTag: true
   } })
+  win.setMenuBarVisibility(false)
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
@@ -27,6 +29,7 @@ function createWindow () {
   } else {
     createProtocol('app')
     // Load the index.html when not in development
+
     win.loadURL('app://./index.html')
   }
 
